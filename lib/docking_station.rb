2960cc552a427820a23f4560b49ce65e437057bb
AlexiaConstant <alexia.constant@yahoo.com>
2021-07-20 14:09:40 +0100

    updated rspec and docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,21 @@
 require_relative "bike"
 class DockingStation
-  attr_reader :storage
+  #attr_reader :storage
+  attr_reader :bike
   
-  def initialize
-    @storage = []
-  end
+  #def initialize
+    #@storage = []
+  #end
   
   def release_bike
-    bike = Bike.new
+    fail 'no bikes available' unless @bike
+    @bike
+    #bike = Bike.new
+  end
+  
+  def docking_station(bike)
+  @bike = bike
   end
 
-  
+
 end
